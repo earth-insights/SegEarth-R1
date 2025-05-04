3514d883a5630e4766cf7b1108a07436e9c6e6f9
Carl <144805353+xcarl1@users.noreply.github.com>
2025-05-04 18:04:12 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,6 +1,6 @@
 export CUDA_VISIBLE_DEVICES=0
 python SegEarthR1/eval_and_test/eval.py \
-  --model_path /root/siton-data-412581749c3f4cfea0d7c972b8742057/proj/SegEarth-R1-all/checkpoint/EarthReason/PSALM_Reason_SparseConv \
+  --model_path your_model_path \
   --data_split "val" \
   --version "llava_phi" \
   --mask_config "SegEarthR1/mask_config/maskformer2_swin_base_384_bs16_50ep.yaml" \
