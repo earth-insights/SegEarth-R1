a30a7741403fc11b21e1e13a5d6a55204a5e7909
Carl <144805353+xcarl1@users.noreply.github.com>
2025-05-13 20:52:19 +0800

    Update eval.sh

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -5,3 +5,4 @@ python segearth_r1/eval_and_test/eval.py \
   --version "llava_phi" \
   --mask_config "segearth_r1/mask_config/maskformer2_swin_base_384_bs16_50ep.yaml" \
   --dataset_type "EarthReason" \
+  # --vis_path "your_visualization_path" \
